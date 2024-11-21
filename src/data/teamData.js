d0d31e8130438156c2e7f9bdf6d8341ee2a9e27a
hci-lab-dcsug <hci-lab-dcsug@gmail.com>
2024-11-21 17:32:04 +0000

    Wiafe-Akenteng fix

--- src/data/teamData.js
+++ src/data/teamData.js
@@ -13,15 +13,16 @@ const teamData = {
     about:
       "Professor Isaac Wiafe is a distinguished scholar whose pioneering research focuses on the intersection of technology and human behavior, particularly in the areas of virtual reality and persuasive technologies. He is recognized for his innovative studies on how these technologies, designed to change attitudes or behaviors, can promote healthier lifestyles, enhance learning outcomes, and encourage sustainable practices. By leveraging immersive virtual environments and intelligent systems, he creates engaging experiences that drive meaningful behavioral changes.",
   },
-    codirector: {
+  codirector: {
     name: "Jamal-Deen Abdulai",
     title: "Professor & Lab Director",
     image: "/prof_jamal.jpg",
     description: [
-        "- Computer Networking",
-        "- Wireless Communication Systems",
-        "- Sensor Networks",
-        "- AI and Machine Learning",],
+      "- Computer Networking",
+      "- Wireless Communication Systems",
+      "- Sensor Networks",
+      "- AI and Machine Learning",
+    ],
     about:
       "Professor Jamal-Deen is a distinguished scholar whose pioneering research focuses on the intersection of wireless networks and artificial intelligence, particularly in the areas of virtual reality and persuasive technologies. He is recognized for his innovative studies on how these technologies, designed to change attitudes or behaviors, can promote healthier lifestyles, enhance learning outcomes, and encourage sustainable practices. By leveraging immersive virtual environments and intelligent systems, he creates engaging experiences that drive meaningful behavioral changes.",
   },
@@ -62,7 +63,7 @@ const teamData = {
         "Computer Vision, Automatic Speech Recognition, Natural Language Processing.",
     },
     {
-      name: "Akosua Nyarkoa Akenteng-Wiafe",
+      name: "Akosua Nyarkoa Wiafe-Akenteng",
       title: "MSc Student",
       image: "/akosua.jpeg",
       expertise:
