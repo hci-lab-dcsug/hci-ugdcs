48a3ebd3a1de64bb5d2622ab92f2485dc270af4e
devkyle4 <devkyle4@gmail.com>
2025-01-23 21:26:24 +0000

    small chance

--- src/components/pages/TeamPage.js
+++ src/components/pages/TeamPage.js
@@ -5,40 +5,68 @@ import teamData from "../../data/teamData";
 import { Link } from "react-router-dom"; // For navigation if using routes
 
 function TeamPage() {
-  const { team, director } = teamData;
+  const { team, staffs } = teamData;
+  const currentMembers = team.filter((member) => member.status === "current");
+  const pastMembers = team.filter((member) => member.status === "past");
 
   return (
     <div className="team-page">
-      {/* Director Section */}
-      <section className="director-section">
-      <h2 className="team-section-title">Lab Director</h2>
-        <div className="directors-grid">
-          {/* Director Card */}
-          <div className="director-card">
-            <img
-              src={director.image}
-              alt={director.name}
-              className="director-image"
-            />
-            <h3>{director.name}</h3>
-            <h4>{director.title}</h4>
-            <ul>
-              {director.description.slice(0, 3).map((item, index) => (
-                <li key={index}>{item}</li>
-              ))}
-            </ul>
-            <Link to="https://scholar.google.com/citations?user=rTdeBg0AAAAJ&hl=en&oi=ao" target="_blank" className="read-more-link">
-              Read More
-            </Link>
-          </div>
+      {/* staffs Section */}
+      <section className="staffs-section">
+        <h2 className="team-section-title">Lab Staff</h2>
+        <div className="team-grid staffs-grid">
+          {/* staffs Card */}
+          {staffs.map((staff, index) => (
+            <div key={index} className="staffs-card">
+              <img
+                src={staff.image}
+                alt={staff.name}
+                className="staffs-image"
+              />
+              <h3>{staff.name}</h3>
+              <h4>{staff.title}</h4>
+              <ul>
+                {staff.description.map((item, index) => (
+                  <li key={index}>{item}</li>
+                ))}
+              </ul>
+              <Link
+                to="https://scholar.google.com/citations?user=rTdeBg0AAAAJ&hl=en&oi=ao"
+                target="_blank"
+                className="read-more-link"
+              >
+                Read More
+              </Link>
+            </div>
+          ))}
         </div>
       </section>
 
       {/* Research Team Section */}
+
+      {/* Current Members */}
       <section className="team-section">
         <h2 className="team-section-title">Research Team</h2>
+        <h4 className="member-status"> Current Members</h4>
         <div className="team-grid">
-          {team.map((member, index) => (
+          {currentMembers.map((member, index) => (
+            <TeamCard
+              key={index}
+              image={member.image}
+              name={member.name}
+              title={member.title}
+              expertise={member.expertise}
+            />
+          ))}
+        </div>
+      </section>
+
+      {/* Past Members */}
+      <section className="team-section">
+        <h2 className="team-section-title">Research Team</h2>
+        <h4 className="member-status"> Past Members</h4>
+        <div className="team-grid">
+          {pastMembers.map((member, index) => (
             <TeamCard
               key={index}
               image={member.image}
--- src/data/teamData.js
+++ src/data/teamData.js
@@ -1,49 +1,42 @@
 const teamData = {
-  director: {
-    name: "Isaac Wiafe",
+  staffs: [
+    {name: "Isaac Wiafe",
     title: "Associate Professor of Information Technology & Lab Director",
     image: `${process.env.PUBLIC_URL}/prof.jpg`,
     description: [
-      "- Persuasive Technologies",
-      "- IT for Behaviour Change",
       "- Systems Analysis & Design",
       "- Large Language Modelling",
-      "- Extended Reality"
+      "- Persuasive Technologies",
+      "- IT for Behaviour Change",
+      "- Extended Reality",
     ],
     about:
       "Professor Isaac Wiafe is a distinguished scholar whose pioneering research focuses on the intersection of technology and human behavior, particularly in the areas of virtual reality and persuasive technologies. He is recognized for his innovative studies on how these technologies, designed to change attitudes or behaviors, can promote healthier lifestyles, enhance learning outcomes, and encourage sustainable practices. By leveraging immersive virtual environments and intelligent systems, he creates engaging experiences that drive meaningful behavioral changes.",
   },
-  codirector: {
+  {
     name: "Jamal-Deen Abdulai",
     title: "Associate Professor",
     image: `${process.env.PUBLIC_URL}/prof_jamal.jpg`,
     description: [
-      "- Computer Networking",
       "- Wireless Communication Systems",
-      "- Sensor Networks",
       "- AI and Machine Learning",
+      "- Computer Networking",
+      "- Sensor Networks",
+      "- Photonics",
     ],
     about:
       "Professor Jamal-Deen is a distinguished scholar whose pioneering research focuses on the intersection of wireless networks and artificial intelligence, particularly in the areas of virtual reality and persuasive technologies. He is recognized for his innovative studies on how these technologies, designed to change attitudes or behaviors, can promote healthier lifestyles, enhance learning outcomes, and encourage sustainable practices. By leveraging immersive virtual environments and intelligent systems, he creates engaging experiences that drive meaningful behavioral changes.",
   },
-
+],
   team: [
-    {
-      name: "Jamal-Deen Abdulai",
-      title: "Associate Professor",
-      image: `${process.env.PUBLIC_URL}/prof_jamal.jpg`,
-      expertise: [
-        "Computer Networking, Wireless Communication Systems, Sensor Networks, AI and Machine Learning",
-      ],
-      about:
-        "Professor Jamal-Deen is a distinguished scholar whose pioneering research focuses on the intersection of wireless networks and artificial intelligence, particularly in the areas of virtual reality and persuasive technologies. He is recognized for his innovative studies on how these technologies, designed to change attitudes or behaviors, can promote healthier lifestyles, enhance learning outcomes, and encourage sustainable practices. By leveraging immersive virtual environments and intelligent systems, he creates engaging experiences that drive meaningful behavioral changes.",
-    },
+
     {
       name: "Akon Ekpezu",
       title: "Research Assistant, PhD Student",
       image: `${process.env.PUBLIC_URL}/akon.jpg`,
       expertise:
         "Persuasive Technologies, Natural Language Processing, Automatic Speech Recognition, Large Language Models",
+      status: "current",
     },
     {
       name: "Mark Atta Mensah",
@@ -51,19 +44,31 @@ const teamData = {
       image: `${process.env.PUBLIC_URL}/mark.jpeg`,
       expertise:
         "Persuasive Technologies, Natural Language Processing, Automatic Speech Recognition, Large Language Models",
+      status: "current",
     },
     {
       name: "Elikem Doe Atsapo",
       title: "Research Assistant, MPhil Student",
       image: `${process.env.PUBLIC_URL}/elikem.jpeg`,
-      expertise: "Virtual Reality, Automatic Speech Recognition, Natural Language Processing",
+      expertise:
+        "Virtual Reality, Automatic Speech Recognition, Natural Language Processing",
+      status: "past",
+    },
+    {
+      name: "Elikem Doe Atsapo",
+      title: "Research Assistant, MPhil Student",
+      image: `${process.env.PUBLIC_URL}/elikem.jpeg`,
+      expertise:
+        "Virtual Reality, Automatic Speech Recognition, Natural Language Processing",
+      status: "past",
     },
     {
       name: "Sumaya Salihs Ahmed",
       title: "Research Assistant, MSc Student",
       image: `${process.env.PUBLIC_URL}/sumaya.jpeg`,
       expertise:
-        "Automatic Speech Recognition, Natural Language Processing, Large Language Models"
+        "Automatic Speech Recognition, Natural Language Processing, Large Language Models",
+      status: "current",
     },
     {
       name: "Akosua Nyarkoa Wiafe-Akenteng",
@@ -71,6 +76,7 @@ const teamData = {
       image: `${process.env.PUBLIC_URL}/akosua.jpeg`,
       expertise:
         "Automatic Speech Recognition, Natural Language Processing, Large Language Models",
+      status: "current",
     },
     {
       name: "Evans Kwasi",
@@ -78,6 +84,7 @@ const teamData = {
       image: `${process.env.PUBLIC_URL}/evans.jpeg`,
       expertise:
         "Automatic Speech Recognition, Natural Language Processing, Large Language Models",
+      status: "current",
     },
     {
       name: "Fiifi Baffoe Payin Winful",
@@ -85,6 +92,7 @@ const teamData = {
       image: `${process.env.PUBLIC_URL}/payin.jpeg`,
       expertise:
         "Automatic Speech Recognition, Virtual Reality, Natural Language Processing, Large Language Models",
+      status: "current",
     },
     {
       name: "Kafui Solaga",
@@ -92,6 +100,7 @@ const teamData = {
       image: `${process.env.PUBLIC_URL}/payin.jpeg`,
       expertise:
         "Automatic Speech Recognition, Virtual Reality, Natural Language Processing, Large Language Models",
+      status: "current",
     },
   ],
 };
